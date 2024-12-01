7d3d2daff040dc60e74b0b8d6573071c6dacfc89
Mustafa Selim Oral <mustafaselimoral@gmail.com>
2024-12-01 19:57:07 +0300

    Initial project setup

---
+++ 2024/rust/daily-template/src/bin/part1.rs
@@ -0,0 +1,12 @@
+use {{crate_name}}::part1::process;
+
+#[tracing::instrument]
+fn main() -> anyhow::Result<()> {
+    use anyhow::Context;
+    tracing_subscriber::fmt::init();
+
+    let file = include_str!("../../input1.txt");
+    let result = process(file).context("process part 1")?;
+    println!("{}", result);
+    Ok(())
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Advent of Code
+
+Solutions for the [Advent of Code](https://adventofcode.com/)
+
+
+## Rust
+
+I use [just](https://github.com/casey/just) command runner to simply things.
+You can install just using `cargo install just`. Refer to project repo for other installation options.
