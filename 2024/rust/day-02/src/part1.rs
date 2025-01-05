d990597e1b91cf11448353eaf760b4b3cdef4230
Mustafa Selim Oral <mustafaselimoral@gmail.com>
2025-01-05 14:57:21 +0300

    add day2 solution

--- 2024/rust/day-02/src/part1.rs
+++ 2024/rust/day-02/src/part1.rs
@@ -1,6 +1,68 @@
-#[tracing::instrument]
+use nom::{
+    character::complete::{self, newline, space1},
+    multi::separated_list1,
+    IResult,
+};
+
+enum Direction {
+    Up,
+    Down,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+enum Safety {
+    Safe,
+    Unsafe,
+}
+
+#[tracing::instrument(ret)]
+fn check_safety(report: &Report) -> Safety {
+    use itertools::Itertools;
+
+    let mut direction: Option<Direction> = None;
+    for (a, b) in report.iter().tuple_windows() {
+        let diff = a - b;
+        if !(1..=3).contains(&diff.abs()) {
+            return Safety::Unsafe;
+        }
+        match diff.signum() {
+            -1 => match direction {
+                Some(Direction::Up) => return Safety::Unsafe,
+                Some(Direction::Down) => (),
+                None => {
+                    direction = Some(Direction::Down);
+                }
+            },
+            1 => match direction {
+                Some(Direction::Up) => (),
+                Some(Direction::Down) => return Safety::Unsafe,
+                None => {
+                    direction = Some(Direction::Up);
+                }
+            },
+            0 => return Safety::Unsafe,
+            _ => panic!("should never have a non -1, 1, 0 number"),
+        }
+    }
+    Safety::Safe
+}
+
+type Report = Vec<i32>;
+
+fn parse(input: &str) -> IResult<&str, Vec<Report>> {
+    separated_list1(newline, separated_list1(space1, complete::i32))(input)
+}
+
+#[tracing::instrument(skip(input))]
 pub fn process(input: &str) -> anyhow::Result<String> {
-    todo!("day 01 - part 1");
+    let (_, reports) = parse(input).map_err(|err| anyhow::anyhow!("Parse failed {}", err))?;
+
+    let safe_report_count = reports
+        .iter()
+        .map(check_safety)
+        .filter(|safety| safety == &Safety::Safe)
+        .count();
+    Ok(safe_report_count.to_string())
 }
 
 #[cfg(test)]
@@ -9,9 +71,15 @@ mod tests {
 
     #[test]
     fn test_process() -> anyhow::Result<()> {
-        todo!("haven't built test yet");
-        let input = indoc::indoc!(r#""#);
-        assert_eq!("", process(input)?);
+        let input = indoc::indoc!(
+            r#"7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9"#
+        );
+        assert_eq!("2", process(input)?);
         Ok(())
     }
 }
--- 2024/rust/day-02/src/part2.rs
+++ 2024/rust/day-02/src/part2.rs
@@ -1,6 +1,77 @@
-#[tracing::instrument]
+use nom::{
+    character::complete::{self, line_ending, space1},
+    multi::separated_list1,
+    IResult,
+};
+
+enum Direction {
+    Increasing,
+    Decreasing,
+}
+
+#[tracing::instrument(ret)]
+fn check_safety(report: &Report) -> Result<(), String> {
+    use itertools::Itertools;
+    let mut direction: Option<Direction> = None;
+
+    for (a, b) in report.iter().tuple_windows() {
+        let diff = a - b;
+        if !(1..=3).contains(&diff.abs()) {
+            return Err(format!("{}, {} diff value is {}", a, b, diff.abs()))
+        }
+        match diff.signum() {
+            -1 => match direction {
+                Some(Direction::Increasing) => return Err(format!("{}, {} switched to increasing", a, b)),
+                Some(Direction::Decreasing) => (),
+                None => {
+                    direction = Some(Direction::Decreasing);
+                }
+            },
+            1 => match direction {
+                Some(Direction::Increasing) => (),
+                Some(Direction::Decreasing) => return Err(format!("{}, {} switched to decreasing", a, b)),
+                None => {
+                    direction = Some(Direction::Increasing);
+                }
+            },
+            0 => return Err(format!("{}, {} diff was 0", a, b)),
+            _ => panic!("should never have a non -1, 1, 0 number"),
+        }
+    }
+    Ok(())
+}
+
+type Report = Vec<i32>;
+
+fn parse(input: &str) -> IResult<&str, Vec<Report>> {
+    separated_list1(line_ending, separated_list1(space1, complete::i32))(input)
+}
+
+#[tracing::instrument(skip(input))]
 pub fn process(input: &str) -> anyhow::Result<String> {
-    todo!("day 01 - part 2");
+    let (_, reports) = parse(input).map_err(|err| anyhow::anyhow!("Parse failed {}", err))?;
+
+    let safe_report_count = reports
+        .iter()
+        .filter(|report| {
+            if check_safety(report).is_ok() {
+                return true;
+            }
+            
+            // Check if there is a safe version of the report
+            for index in 0..report.len() {
+                let mut amended_report = (*report).clone();
+                amended_report.remove(index);
+                if check_safety(&amended_report).is_ok() {
+                    return true;
+                } else {
+                    continue;
+                }
+            }
+            return false;
+        })
+        .count();
+    Ok(safe_report_count.to_string())
 }
 
 #[cfg(test)]
@@ -9,9 +80,17 @@ mod tests {
 
     #[test]
     fn test_process() -> anyhow::Result<()> {
-        todo!("haven't built test yet");
-        let input = indoc!(r#""#);
-        assert_eq!("", process(input)?);
+        let input = indoc::indoc!(
+            r#"
+        7 6 4 2 1
+        1 2 7 8 9
+        9 7 6 2 1
+        1 3 2 4 5
+        8 6 4 4 1
+        1 3 6 7 9
+        "#
+        );
+        assert_eq!("4", process(input)?);
         Ok(())
     }
 }
