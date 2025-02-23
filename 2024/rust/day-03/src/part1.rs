cf3a51d500b2f860af07f6109c6d65b05f240934
Mustafa Selim Oral <mustafaselimoral@gmail.com>
2025-02-23 18:31:48 +0300

    add day3 solution

--- 2024/rust/day-03/src/part1.rs
+++ 2024/rust/day-03/src/part1.rs
@@ -1,6 +1,43 @@
+use nom::{
+    bytes::complete::tag,
+    character::complete::{self, anychar},
+    multi::{many1, many_till},
+    sequence::{delimited, separated_pair},
+    IResult, Parser,
+};
+
 #[tracing::instrument]
 pub fn process(input: &str) -> anyhow::Result<String> {
-    todo!("day 01 - part 1");
+    let (_input, instructions) =
+        parse(input).map_err(|e| anyhow::anyhow!("failed to parse {}]", e))?;
+
+    let result: u32 = instructions
+        .iter()
+        .map(|inst| match inst {
+            Instruction::Mul(a, b) => a * b,
+        })
+        .sum();
+
+    Ok(result.to_string())
+}
+
+#[derive(Debug)]
+enum Instruction {
+    Mul(u32, u32),
+}
+
+fn instruction(input: &str) -> IResult<&str, Instruction> {
+    let (input, _) = tag("mul")(input)?;
+    let (input, pair) = delimited(
+        tag("("),
+        separated_pair(complete::u32, tag(","), complete::u32),
+        tag(")"),
+    )(input)?;
+    Ok((input, Instruction::Mul(pair.0, pair.1)))
+}
+
+fn parse(input: &str) -> IResult<&str, Vec<Instruction>> {
+    many1(many_till(anychar, instruction).map(|(_ignore, inst)| inst))(input)
 }
 
 #[cfg(test)]
@@ -9,9 +46,8 @@ mod tests {
 
     #[test]
     fn test_process() -> anyhow::Result<()> {
-        todo!("haven't built test yet");
-        let input = indoc::indoc!(r#""#);
-        assert_eq!("", process(input)?);
+        let input = indoc::indoc!(r#"xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"#);
+        assert_eq!("161", process(input)?);
         Ok(())
     }
 }
--- 2024/rust/day-03/src/part2.rs
+++ 2024/rust/day-03/src/part2.rs
@@ -1,6 +1,62 @@
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::{self, anychar},
+    combinator::value,
+    multi::{many1, many_till},
+    sequence::{delimited, separated_pair},
+    IResult, Parser,
+};
+
 #[tracing::instrument]
 pub fn process(input: &str) -> anyhow::Result<String> {
-    todo!("day 01 - part 2");
+    let (_input, instructions) = parse(input).map_err(|e| anyhow::anyhow!("parse failed {}", e))?;
+
+    fn step((can_process, acc): (bool, u32), inst: &Instruction) -> (bool, u32) {
+        match inst {
+            Instruction::Mul(a, b) => {
+                if can_process {
+                    (can_process, acc + a * b)
+                } else {
+                    (can_process, acc)
+                }
+            },
+            Instruction::Do => (true, acc),
+            Instruction::Dont => (false, acc),
+        }
+    }
+
+    let (_, result) = instructions.iter().fold((true, 0), step);
+    Ok(result.to_string())
+}
+
+#[derive(Clone, Debug)]
+enum Instruction {
+    Mul(u32, u32),
+    Do,
+    Dont,
+}
+
+fn mul(input: &str) -> IResult<&str, Instruction> {
+    let (input, _) = tag("mul")(input)?;
+    let (input, pair) = delimited(
+        tag("("),
+        separated_pair(complete::u32, tag(","), complete::u32),
+        tag(")"),
+    )(input)?;
+    Ok((input, Instruction::Mul(pair.0, pair.1)))
+}
+
+fn instruction(input: &str) -> IResult<&str, Instruction> {
+    alt((
+        value(Instruction::Dont, tag("don't()")),
+        value(Instruction::Do, tag("do()")),
+        mul,
+    ))(input)
+}
+
+fn parse(input: &str) -> IResult<&str, Vec<Instruction>> {
+    many1(many_till(anychar, instruction).map(|(_discard, ins)| ins))(input)
 }
 
 #[cfg(test)]
@@ -9,9 +65,10 @@ mod tests {
 
     #[test]
     fn test_process() -> anyhow::Result<()> {
-        todo!("haven't built test yet");
-        let input = indoc::indoc!(r#""#);
-        assert_eq!("", process(input)?);
+        let input = indoc::indoc!(
+            r#"xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"#
+        );
+        assert_eq!("48", process(input)?);
         Ok(())
     }
 }
