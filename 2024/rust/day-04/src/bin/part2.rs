485543147ce4356a4f4abdf67b9b87e5142c5b08
Mustafa Selim Oral <mustafaselimoral@gmail.com>
2025-02-23 18:34:39 +0300

    chore: add day-04 template

---
+++ 2024/rust/day-04/src/bin/part2.rs
@@ -0,0 +1,12 @@
+use day_04::part2::process;
+
+#[tracing::instrument]
+fn main() -> anyhow::Result<()> {
+    use anyhow::Context;
+    tracing_subscriber::fmt::init();
+
+    let file = include_str!("../../input2.txt");
+    let result = process(file).context("process part 2")?;
+    println!("{}", result);
+    Ok(())
+}
