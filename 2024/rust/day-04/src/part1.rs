1c1d159569f1548411a388c74ca878abbb6d181e
Mustafa Selim Oral <mustafaselimoral@gmail.com>
2025-02-23 22:30:41 +0300

    add day4 solution

--- 2024/rust/day-04/src/part1.rs
+++ 2024/rust/day-04/src/part1.rs
@@ -1,6 +1,50 @@
-#[tracing::instrument]
+use std::collections::HashMap;
+
+use glam::IVec2;
+
+const DIRECTIONS: [[IVec2; 3]; 8] = [
+    [IVec2::new(0, 1), IVec2::new(0, 2), IVec2::new(0, 3)],
+    [IVec2::new(0, -1), IVec2::new(0, -2), IVec2::new(0, -3)],
+    [IVec2::new(1, 1), IVec2::new(2, 2), IVec2::new(3, 3)],
+    [IVec2::new(1, -1), IVec2::new(2, -2), IVec2::new(3, -3)],
+    [IVec2::new(-1, 1), IVec2::new(-2, 2), IVec2::new(-3, 3)],
+    [IVec2::new(-1, -1), IVec2::new(-2, -2), IVec2::new(-3, -3)],
+    [IVec2::new(1, 0), IVec2::new(2, 0), IVec2::new(3, 0)],
+    [IVec2::new(-1, 0), IVec2::new(-2, 0), IVec2::new(-3, 0)],
+];
+
+#[tracing::instrument(skip(input))]
 pub fn process(input: &str) -> anyhow::Result<String> {
-    todo!("day 01 - part 1");
+    let positions: HashMap<IVec2, char> = input
+        .lines()
+        .enumerate()
+        .flat_map(|(y, line)| {
+            line.chars()
+                .enumerate()
+                .map(move |(x, value)| (IVec2::new(x as i32, y as i32), value))
+        })
+        .collect();
+
+    let search_chars = ['M', 'A', 'S'];
+
+    Ok(positions
+        .iter()
+        .filter(|(_position, value)| **value == 'X')
+        .map(|(position, _value)| {
+            DIRECTIONS
+                .iter()
+                .map(|search_char_positions| {
+                    search_char_positions
+                        .iter()
+                        .map(|offset| positions.get(&(position + offset)))
+                        .enumerate()
+                        .all(|(index, value)| search_chars.get(index) == value)
+                })
+                .filter(|b| *b)
+                .count()
+        })
+        .sum::<usize>()
+        .to_string())
 }
 
 #[cfg(test)]
@@ -9,9 +53,21 @@ mod tests {
 
     #[test]
     fn test_process() -> anyhow::Result<()> {
-        todo!("haven't built test yet");
-        let input = indoc::indoc!(r#""#);
-        assert_eq!("", process(input)?);
+        let input = indoc::indoc!(
+            r#"
+        MMMSXXMASM
+        MSAMXMSMSA
+        AMXSXMAAMM
+        MSAMASMSMX
+        XMASAMXAMM
+        XXAMMXXAMA
+        SMSMSASXSS
+        SAXAMASAAA
+        MAMMMXMMMM
+        MXMXAXMASX
+        "#
+        );
+        assert_eq!("18", process(input)?);
         Ok(())
     }
 }
--- 2024/rust/day-04/src/part2.rs
+++ 2024/rust/day-04/src/part2.rs
@@ -1,6 +1,47 @@
-#[tracing::instrument]
+use std::collections::HashMap;
+
+use glam::IVec2;
+
+const DIRECTIONS: [[IVec2; 2]; 4] = [
+    [IVec2::new(-1, -1), IVec2::new(1, 1)],
+    [IVec2::new(-1, 1), IVec2::new(1, -1)],
+    [IVec2::new(1, 1), IVec2::new(-1, -1)],
+    [IVec2::new(1, -1), IVec2::new(-1, 1)],
+];
+
+#[tracing::instrument(skip(input))]
 pub fn process(input: &str) -> anyhow::Result<String> {
-    todo!("day 01 - part 2");
+    let positions: HashMap<IVec2, char> = input
+        .lines()
+        .enumerate()
+        .flat_map(|(y, line)| {
+            line.chars()
+                .enumerate()
+                .map(move |(x, value)| (IVec2::new(x as i32, y as i32), value))
+        })
+        .collect();
+
+    let search_chars = ['M', 'S'];
+
+    Ok(positions
+        .iter()
+        .filter(|(_positions, value)| **value == 'A')
+        .filter(|(position, _value)| {
+            DIRECTIONS
+                .iter()
+                .map(|search_char_positions| {
+                    search_char_positions
+                        .iter()
+                        .map(|pos| positions.get(&(*position + pos)))
+                        .enumerate()
+                        .all(|(index, value)| search_chars.get(index) == value)
+                })
+                .filter(|b| *b)
+                .count()
+                == 2
+        })
+        .count()
+        .to_string())
 }
 
 #[cfg(test)]
@@ -9,9 +50,21 @@ mod tests {
 
     #[test]
     fn test_process() -> anyhow::Result<()> {
-        todo!("haven't built test yet");
-        let input = indoc::indoc!(r#""#);
-        assert_eq!("", process(input)?);
+        let input = indoc::indoc!(
+            r#"
+        .M.S......
+        ..A..MSMS.
+        .M.S.MAA..
+        ..A.ASMSM.
+        .M.S.M....
+        ..........
+        S.S.S.S.S.
+        .A.A.A.A..
+        M.M.M.M.M.
+        ..........
+        "#
+        );
+        assert_eq!("9", process(input)?);
         Ok(())
     }
 }
